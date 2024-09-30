dfbd86b5224c444e1f2f31e8c6a2e141e5b4b7d0
Onur45500 <onur.akmese@viacesi.fr>
2024-09-30 09:09:41 +0200

    Initialized the project

---
+++ csharp-net-wpf-validations-in-forms/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace csharp_net_wpf_validations_in_forms
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
